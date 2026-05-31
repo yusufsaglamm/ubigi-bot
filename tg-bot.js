777589409debe480d5e20b96405fc339ffe47568
Yusuf <78987888+yusufsaglamm@users.noreply.github.com>
2026-05-31 17:50:53 +0300

    Add files via upload

--- tg-bot.js
+++ tg-bot.js
@@ -15,7 +15,10 @@ if (token === 'BURAYA_TELEGRAM_BOT_TOKENINIZI_YAZIN') {
 
 // Sadece sizin kullanmanızı sağlamak için yetkili Chat ID (Telegram Kullanıcı ID'niz)
 // Kendi ID'nizi öğrenmek için Telegram'da @userinfobot 'a mesaj atabilirsiniz.
-const ALLOWED_USER_ID = process.env.ALLOWED_USER_ID || '6698095710' || '8098200755';
+const ALLOWED_USER_IDS = ['6698095710', '8098200755'];
+if (process.env.ALLOWED_USER_ID) {
+    ALLOWED_USER_IDS.push(process.env.ALLOWED_USER_ID);
+}
 
 
 // Bot'u polling (sürekli dinleme) modunda başlat
@@ -294,7 +297,7 @@ bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     
     // Yetki Kontrolü
-    if (ALLOWED_USER_ID !== 'BURAYA_KENDI_CHAT_IDNIZI_YAZIN' && chatId.toString() !== ALLOWED_USER_ID) {
+    if (!ALLOWED_USER_IDS.includes(chatId.toString())) {
         return bot.sendMessage(chatId, `🚫 Yetkisiz erişim! Bu bot özeldir ve başkası tarafından kullanılamaz.\nSizin Chat ID'niz: ${chatId}`);
     }
 
@@ -305,7 +308,7 @@ bot.onText(/\/uret (\d+)/, async (msg, match) => {
     const chatId = msg.chat.id;
     
     // Yetki Kontrolü
-    if (ALLOWED_USER_ID !== 'BURAYA_KENDI_CHAT_IDNIZI_YAZIN' && chatId.toString() !== ALLOWED_USER_ID) {
+    if (!ALLOWED_USER_IDS.includes(chatId.toString())) {
         return bot.sendMessage(chatId, `🚫 Yetkisiz işlem denemesi.`);
     }
 
